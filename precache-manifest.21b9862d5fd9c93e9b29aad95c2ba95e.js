6d143806f51573558c70bfdec207d770fe112162
elena <ebassist@live.com>
2019-04-02 22:09:45 +1300

    Updates

---
+++ precache-manifest.21b9862d5fd9c93e9b29aad95c2ba95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47a0633b560beec9dbe8",
+    "url": "/weather-me-home/static/css/main.51d0dd2b.chunk.css"
+  },
+  {
+    "revision": "47a0633b560beec9dbe8",
+    "url": "/weather-me-home/static/js/main.534055d1.chunk.js"
+  },
+  {
+    "revision": "aad521fbd998caf89904",
+    "url": "/weather-me-home/static/js/runtime~main.d66f875d.js"
+  },
+  {
+    "revision": "e1965f416cc24fe07051",
+    "url": "/weather-me-home/static/js/2.ccfa2e17.chunk.js"
+  },
+  {
+    "revision": "87e72264211d54236587c496d0aaed29",
+    "url": "/weather-me-home/static/media/me.87e72264.jpg"
+  },
+  {
+    "revision": "c51aab0229745425a8c9a1c155b3b6f7",
+    "url": "/weather-me-home/index.html"
+  }
+];
